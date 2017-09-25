f062ca227f6ef6d407d1426910003d9df5c11740
Andrew Letson <velinath@users.noreply.github.com>
2017-09-25 17:29:18 -0400

    Update 2017_09_25_99_world.sql

--- sql/updates/world/3.3.5/2017_09_25_99_world.sql
+++ sql/updates/world/3.3.5/2017_09_25_99_world.sql
@@ -8,7 +8,7 @@ INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionTex
 UPDATE `creature_template` SET `AIName` = 'SmartAI', `ScriptName` = '' WHERE `entry` IN (17900,17901);
 
 -- Create SAI
-DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 17900);
+DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid IN (17900, 17901));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (17900, 0, 0, 4, 62, 0, 100, 0, 7559, 0, 0, 0, 11, 31808, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Ashyen - On Gossip Option 0 Selected - Cast \'Mark of Lore\''),
 (17900, 0, 1, 4, 62, 0, 100, 0, 7559, 0, 0, 0, 11, 31810, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Ashyen - On Gossip Option 0 Selected - Cast \'Mark of Lore\''),
